5ae930742f4edd429ff3532ceb612bdae766d0fe
Benjamin DOMERGUE <thebenjiman@TheHairyMan.local>
2012-10-06 16:31:29 +0200

    Initial Commit

---
+++ C++FirstSteps/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++FirstSteps
+//
+//  Created by Benjamin DOMERGUE on 06/10/12.
+//  Copyright (c) 2012 Benjamin DOMERGUE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
